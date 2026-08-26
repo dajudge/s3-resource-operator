46448e94d149107873b031fd1d6698f794b7f7c2
Alex Stockinger <mail@alexstockinger.de>
2026-08-26 16:55:00 +0200

    Rename S3Instance spec to backend spec

---
+++ src/main/java/com/dajudge/s3operator/api/S3BackendSpec.java
@@ -0,0 +1,20 @@
+package com.dajudge.s3operator.api;
+
+public class S3BackendSpec {
+    private String provider = "versity";
+    private String endpoint;
+    private SecretRef adminCredentialsSecretRef;
+
+    public String getProvider() { return provider; }
+    public void setProvider(String provider) { this.provider = provider; }
+    public String getEndpoint() { return endpoint; }
+    public void setEndpoint(String endpoint) { this.endpoint = endpoint; }
+    public SecretRef getAdminCredentialsSecretRef() { return adminCredentialsSecretRef; }
+    public void setAdminCredentialsSecretRef(SecretRef adminCredentialsSecretRef) { this.adminCredentialsSecretRef = adminCredentialsSecretRef; }
+
+    public static class SecretRef {
+        private String name;
+        public String getName() { return name; }
+        public void setName(String name) { this.name = name; }
+    }
+}
