a2818b1806ea35aafb47f5cc325ef82c2a59cb34
Alex Stockinger <mail@alexstockinger.de>
2026-08-26 16:43:08 +0200

    api: add standard conditions to bucket status

--- src/main/java/com/dajudge/s3operator/api/S3BucketStatus.java
+++ src/main/java/com/dajudge/s3operator/api/S3BucketStatus.java
@@ -1,11 +1,16 @@
 package com.dajudge.s3operator.api;
 
-public class S3BucketStatus {
-    private String state;
-    private String message;
+import io.fabric8.kubernetes.api.model.Condition;
 
-    public String getState() { return state; }
-    public void setState(String state) { this.state = state; }
-    public String getMessage() { return message; }
-    public void setMessage(String message) { this.message = message; }
+import java.util.ArrayList;
+import java.util.List;
+
+public class S3BucketStatus {
+    private Long observedGeneration;
+    private List<Condition> conditions = new ArrayList<>();
+
+    public Long getObservedGeneration() { return observedGeneration; }
+    public void setObservedGeneration(Long observedGeneration) { this.observedGeneration = observedGeneration; }
+    public List<Condition> getConditions() { return conditions; }
+    public void setConditions(List<Condition> conditions) { this.conditions = conditions; }
 }
