b9c00a97b94485fddd93fabf70159ea4eb800593
Alex Stockinger <mail@alexstockinger.de>
2026-08-26 16:55:21 +0200

    Rename instanceRef to backendRef

--- src/main/java/com/dajudge/s3operator/api/S3UserSpec.java
+++ src/main/java/com/dajudge/s3operator/api/S3UserSpec.java
@@ -1,12 +1,12 @@
 package com.dajudge.s3operator.api;
 
 public class S3UserSpec {
-    private String instanceRef;
+    private String backendRef;
     private String secretName;
     private String role = "user";
 
-    public String getInstanceRef() { return instanceRef; }
-    public void setInstanceRef(String instanceRef) { this.instanceRef = instanceRef; }
+    public String getBackendRef() { return backendRef; }
+    public void setBackendRef(String backendRef) { this.backendRef = backendRef; }
     public String getSecretName() { return secretName; }
     public void setSecretName(String secretName) { this.secretName = secretName; }
     public String getRole() { return role; }
