6d994bd5f1d348a65a4e8c8d837a5ad67caa0f70
Alex Stockinger <mail@alexstockinger.de>
2026-08-28 15:01:12 +0200

    Replace bucket status accessors with Lombok

--- src/main/java/com/dajudge/s3operator/api/S3BucketStatus.java
+++ src/main/java/com/dajudge/s3operator/api/S3BucketStatus.java
@@ -1,16 +1,14 @@
 package com.dajudge.s3operator.api;
 
 import io.fabric8.kubernetes.api.model.Condition;
-
 import java.util.ArrayList;
 import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class S3BucketStatus {
     private Long observedGeneration;
     private List<Condition> conditions = new ArrayList<>();
-
-    public Long getObservedGeneration() { return observedGeneration; }
-    public void setObservedGeneration(Long observedGeneration) { this.observedGeneration = observedGeneration; }
-    public List<Condition> getConditions() { return conditions; }
-    public void setConditions(List<Condition> conditions) { this.conditions = conditions; }
 }
