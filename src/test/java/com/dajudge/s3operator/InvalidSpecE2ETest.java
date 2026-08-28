7dc342f8b4ca6f2e1456d8490d15bafd42f2d43e
Alex Stockinger <mail@alexstockinger.de>
2026-08-28 21:05:25 +0200

    Use K3s E2E test resource

--- src/test/java/com/dajudge/s3operator/InvalidSpecE2ETest.java
+++ src/test/java/com/dajudge/s3operator/InvalidSpecE2ETest.java
@@ -20,7 +20,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
 
 @QuarkusTest
-@QuarkusTestResource(KindVersityTestResource.class)
+@QuarkusTestResource(K3sVersityTestResource.class)
 class InvalidSpecE2ETest {
     private static final String NS = "default";
     private static final Duration TIMEOUT = Duration.ofSeconds(20);
