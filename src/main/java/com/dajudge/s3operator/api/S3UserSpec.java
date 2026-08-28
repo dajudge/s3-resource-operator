15850df20b1fd0d43ccae7e79cf0c73bfb0f974a
Alex Stockinger <mail@alexstockinger.de>
2026-08-28 15:00:44 +0200

    Replace user spec accessors with Lombok

--- src/main/java/com/dajudge/s3operator/api/S3UserSpec.java
+++ src/main/java/com/dajudge/s3operator/api/S3UserSpec.java
@@ -1,14 +1,12 @@
 package com.dajudge.s3operator.api;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class S3UserSpec {
     private String backendRef;
     private String secretName;
     private String role = "user";
-
-    public String getBackendRef() { return backendRef; }
-    public void setBackendRef(String backendRef) { this.backendRef = backendRef; }
-    public String getSecretName() { return secretName; }
-    public void setSecretName(String secretName) { this.secretName = secretName; }
-    public String getRole() { return role; }
-    public void setRole(String role) { this.role = role; }
 }
