3bcf5ea881d47c58ad29585956b8419909728e3e
Alex Stockinger <mail@alexstockinger.de>
2026-08-28 21:04:57 +0200

    Use K3s E2E test resource

--- src/test/java/com/dajudge/s3operator/DependencyWatchE2ETest.java
+++ src/test/java/com/dajudge/s3operator/DependencyWatchE2ETest.java
@@ -7,7 +7,7 @@ import java.time.Duration;
 import org.junit.jupiter.api.Test;
 
 @QuarkusTest
-@QuarkusTestResource(KindVersityTestResource.class)
+@QuarkusTestResource(K3sVersityTestResource.class)
 @TestProfile(LongTimersTestProfile.class)
 class DependencyWatchE2ETest extends OperatorE2ETestSupport {
 
