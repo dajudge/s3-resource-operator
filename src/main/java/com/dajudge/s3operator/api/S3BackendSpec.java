4ec604ce112608f4a412f07a942bb5238828118b
Alex Stockinger <mail@alexstockinger.de>
2026-08-29 09:43:45 +0200

    Describe S3Backend CRD schema

--- src/main/java/com/dajudge/s3operator/api/S3BackendSpec.java
+++ src/main/java/com/dajudge/s3operator/api/S3BackendSpec.java
@@ -1,5 +1,8 @@
 package com.dajudge.s3operator.api;
 
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import io.fabric8.generator.annotation.Default;
+import io.fabric8.generator.annotation.Required;
 import io.fabric8.kubernetes.api.model.LocalObjectReference;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,7 +10,15 @@ import lombok.Setter;
 @Getter
 @Setter
 public class S3BackendSpec {
+    @Default("versity")
+    @JsonPropertyDescription("S3 provider implementation used by this backend.")
     private String provider = "versity";
+
+    @Required
+    @JsonPropertyDescription("S3 endpoint URL used for administrative and bucket operations.")
     private String endpoint;
+
+    @Required
+    @JsonPropertyDescription("Secret containing the backend administrator accessKey and secretKey.")
     private LocalObjectReference adminCredentialsSecretRef;
 }
