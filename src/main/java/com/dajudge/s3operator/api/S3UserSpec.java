776123a100238d3457297b549666f01947e3c7d1
Alex Stockinger <mail@alexstockinger.de>
2026-08-29 09:43:50 +0200

    Describe S3User CRD schema

--- src/main/java/com/dajudge/s3operator/api/S3UserSpec.java
+++ src/main/java/com/dajudge/s3operator/api/S3UserSpec.java
@@ -1,12 +1,22 @@
 package com.dajudge.s3operator.api;
 
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import io.fabric8.generator.annotation.Default;
+import io.fabric8.generator.annotation.Required;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class S3UserSpec {
+    @Required
+    @JsonPropertyDescription("Name of the S3Backend resource that manages this user.")
     private String backendRef;
+
+    @JsonPropertyDescription("Name of the generated credentials Secret. Defaults to <resource-name>-s3.")
     private String secretName;
+
+    @Default("user")
+    @JsonPropertyDescription("Provider role assigned to the S3 user.")
     private String role = "user";
 }
